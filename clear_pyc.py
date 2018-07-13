b78dcc33f4ed567d58641c3412085d1fafffd33b
Lim <31461860+lim1942@users.noreply.github.com>
2018-07-13 11:18:45 +0800

    Update clear_pyc.py

--- clear_pyc.py
+++ clear_pyc.py
@@ -1,27 +1,43 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Date    : 2017-05-03 18:38:29
-# @Author  : broono (broono@qq.com)
-# @Link    : http://broono.sdf.org
-# @Version : $Id$
+#coding=utf-8
+
+# author:lim
+# date:2018.7.13
+# email:940711277
 
 import os
 import sys
 
 suffixes = ['pyc', 'pyo']
+dirName = ['__pycache__']
 
 
 def clean(root_dir, suffixes=suffixes):
+
     if not os.path.isdir(root_dir):
-        print 'Directory {} not exists, skipped.'.format(root_dir)
+        print ('Directory {} not exists, skipped.'.format(root_dir))
+
     suffixes = map(lambda x: '.' + x, suffixes)
     for root, dirs, files in os.walk(root_dir):
+
+        # clear __pycache__ dir
+        for _dir in dirs: 
+            if any(map(lambda x: _dir.endswith(x), dirName)):
+                try:
+                    fullDirName = os.path.join(root,_dir)
+                    os.system('rd /s /q {}'.format(fullDirName))
+                    print(fullDirName+'  done')
+                except Exception as e:
+                    print ('Failed to delete {}: {}'.format(fullDirName, str(e)))
+                    
+        # clear pyc、pyo file
         for file in files:
             if any(map(lambda x: file.endswith(x), suffixes)):
                 try:
-                    os.remove(os.path.join(root, file))
-                except Exception, e:
-                    print 'Failed to delete {}: {}'.format(file, str(e))
+                    fileName = os.path.join(root, file)
+                    os.remove(fileName)
+                    print(fileName+'  done')
+                except Exception as e:
+                    print ('Failed to delete {}: {}'.format(file, str(e)))
 
 
 def clean_all(root_dirs, suffixes=suffixes):
